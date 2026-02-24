b7b93d9fba442644268c6e12d90eaf4263212ff7
subhajitascent-art <subhajit.ascent@gmail.com>
2026-02-24 17:08:02 -0600

    Refactor email sending logic for daily quotes

--- main.py
+++ main.py
@@ -17,16 +17,17 @@ days_of_week={
     1:'Monday',
     2:'Tuesday',3:'Wednesday',4:'Thursday',5:'Friday',6:'Saturday',7:'Sunday'
 }
-if day_of_week==1:
-    quotes=[]
-    with open(file='./quotes.txt') as f1:
-     quotes=f1.readlines()
-    emails_sub += days_of_week[2]+'!'
-    quote_of_day=random.choice(quotes)
-    with smtplib.SMTP("smtp.gmail.com", port=587) as con:
-        con.starttls()
-        con.login(user=MY_EMAIL,password=MY_PASSWORD)
-        con.sendmail(from_addr=MY_EMAIL,to_addrs=MY_EMAIL,msg=f'Subject: {emails_sub}\n\n {quote_of_day}')
+
+quotes=[]
+with open(file='./quotes.txt') as f1:
+    quotes=f1.readlines()
+emails_sub += days_of_week[day_of_week+1]+'!'
+quote_of_day=random.choice(quotes)
+with smtplib.SMTP("smtp.gmail.com", port=587) as con:
+    con.starttls()
+    con.login(user=MY_EMAIL,password=MY_PASSWORD)
+    con.sendmail(from_addr=MY_EMAIL,to_addrs=MY_EMAIL,msg=f'Subject: {emails_sub}\n\n {quote_of_day}')
+
 
 
 
